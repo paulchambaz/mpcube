5ebeb81362307605b8d1639777eae08b915ec09a
Paul Chambaz <paul.chambaz@tutanota.com>
2024-04-01 17:27:46 +0200

    updated toolchain

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
 
     rust-toolchain = fenixPkgs.fromToolchainFile {
       file = ./rust-toolchain.toml;
-      sha256 = "sha256-+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8=";
+      sha256 = "sha256-3St/9/UKo/6lz2Kfq2VmlzHyufduALpiIKaaKX4Pq0g=";
     };
 
     rustPackage = {
