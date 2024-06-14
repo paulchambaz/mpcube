cf2ef4e3b114dd05929166032cb5754710bb93da
Paul Chambaz <paul.chambaz@tutanota.com>
2024-06-14 12:58:09 +0200

    simplified flake

--- flake.nix
+++ flake.nix
@@ -1,82 +1,65 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix }:
-    let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    fenixPkgs = fenix.packages.x86_64-linux;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      buildPkgs = with pkgs; [
+        pkg-config
+        scdoc
+      ];
 
-    buildPkgs = with pkgs; [
-      scdoc
-      libmpdclient
-    ];
+      libPkgs = with pkgs; [
+        libmpdclient
+      ];
 
-    libPkgs = with pkgs; [
-    ];
+      devPkgs = with pkgs; [
+        just
+        cargo-tarpaulin
+        vhs
+      ];
 
-    devPkgs = with pkgs; [
-      just
-      cargo-tarpaulin
-      vhs
-    ];
+    in
+  {
+    packages.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "mpcube";
+      version = "1.0.0";
+      src = ./.;
+      cargoHash = "";
 
-    environment = with pkgs; {
-    };
-
-    makePkgConfigPath = libPkgs: pkgs.lib.concatStringsSep ":" (map (pkg: "${pkg.dev}/lib/pkgconfig") libPkgs);
-
-    rust-toolchain = fenixPkgs.fromToolchainFile {
-      file = ./rust-toolchain.toml;
-      sha256 = "sha256-opUgs6ckUQCyDxcB9Wy51pqhd0MPGHUVbwRKKPGiwZU=";
-    };
-
-    rustPackage = {
-      pname = manifest.name;
-      version = manifest.version;
-      src = self;
-
-      cargoLock.lockFile = ./Cargo.lock;
-      cargoLock.outputHashes = {
-        "mpd-0.1.0" = "sha256-YVatWNIfSd98shfzgdD5rJ40indfge/2bT54DtJIQ1k=";
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+        outputHashes = {
+          "mpd-0.1.0" = "sha256-YVatWNIfSd98shfzgdD5rJ40indfge/2bT54DtJIQ1k=";
+        };
       };
 
-      nativeBuildInputs = [ rust-toolchain ];
-      buildInputs = buildPkgs ++ libPkgs;
+      nativeBuildInputs = buildPkgs;
+      buildInputs = libPkgs;
 
       configurePhase = ''
         export PATH=${pkgs.lib.makeBinPath buildPkgs}:$PATH
-        export PKG_CONFIG_PATH=${makePkgConfigPath libPkgs}:$PKG_CONFIG_PATH
-        export HOME=$(mktemp -d)
       '';
 
       postInstall = ''
         mkdir -p $out/share/man/man1
         scdoc < mpcube.1.scd > $out/share/man/man1/mpcube.1
       '';
+    };
 
-      meta = with pkgs.lib; {
-        description = manifest.description;
-        homepage = manifest.homepage;
-        license = licenses.gpl3Plus;
-        maintainers = with maintainers; [ paulchambaz ];
-      };
-    } // environment;
+    devShell = pkgs.mkShell {
+        buildInputs = libPkgs ++ buildPkgs ++ devPkgs;
 
-    shell = {
-      buildInputs = [ rust-toolchain ] ++ buildPkgs ++ libPkgs ++ devPkgs;
-    } // environment;
-    in
-  {
-    packages.x86_64-linux.mpcube = pkgs.rustPlatform.buildRustPackage rustPackage;
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.mpcube;
-    devShell.x86_64-linux = pkgs.mkShell shell;
-  };
+        shellHook = ''
+        '';
+    };
+  });
 }
